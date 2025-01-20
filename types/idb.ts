ad94c2aaf9587a4bdd22106c7c1eeb0580e1a740
Chiboy <chiboyekene1@gmail.com>
2025-01-20 23:03:43 +0100

    Separated types from main logic

--- types/idb.ts
+++ types/idb.ts
@@ -1,5 +1,4 @@
 import type { DBSchema } from "idb";
-import { uid } from "uid";
 
 export type User = {
 	uid: string;
@@ -70,43 +69,4 @@ export interface WinWebSchema extends DBSchema {
 			user: string
 		}
 	}
-}
-
-
-const defaultUser: User = {
-	avatar: null,
-	fullName: "Default",
-	isCurrent: true,
-	password: "1234",
-	uid: uid(),
-	userName: "User",
-};
-
-const defaultApps = [
-	'File Explorer',
-	'Microsoft Store',
-];
-
-export async function refreshDB() {
-	const userStore = useUser();
-	// deleteAllUsers(tx);
-	const _tx = idb.transaction(idb.objectStoreNames, 'readwrite');
-	const promises = Array.from(idb.objectStoreNames).map((name) => _tx.objectStore(name).clear())
-	promises.push(_tx.done)
-	await Promise.all(promises);
-
-	const tx = idb.transaction(idb.objectStoreNames, "readwrite");
-	const users = tx.objectStore("users");
-	const apps = tx.objectStore('apps')
-	await Promise.all([
-		users.put(defaultUser),
-		apps.put({
-			installedApps: defaultApps
-		}),
-		tx.done
-	])
-
-	userStore.$patch({
-		currentUser: defaultUser,
-	});
 }
--- utils/idb/index.ts
+++ utils/idb/index.ts
@@ -2,6 +2,7 @@ import {
 	openDB,
 } from "idb";
 import type { WinWebSchema } from "../../types/idb";
+import { uid } from "uid";
 
 export const idb = await openDB<WinWebSchema>("winweb", 1, {
 	upgrade(database) {
@@ -38,4 +39,43 @@ export const idb = await openDB<WinWebSchema>("winweb", 1, {
 
 export async function isDBAvalaible() {
 	return idb.count('users').then(a => a > 0);
+}
+
+
+const defaultUser: User = {
+	avatar: null,
+	fullName: "Default",
+	isCurrent: true,
+	password: "1234",
+	uid: uid(),
+	userName: "User",
+};
+
+const defaultApps = [
+	'File Explorer',
+	'Microsoft Store',
+];
+
+export async function refreshDB() {
+	const userStore = useUser();
+	// deleteAllUsers(tx);
+	const _tx = idb.transaction(idb.objectStoreNames, 'readwrite');
+	const promises = Array.from(idb.objectStoreNames).map((name) => _tx.objectStore(name).clear())
+	promises.push(_tx.done)
+	await Promise.all(promises);
+
+	const tx = idb.transaction(idb.objectStoreNames, "readwrite");
+	const users = tx.objectStore("users");
+	const apps = tx.objectStore('apps')
+	await Promise.all([
+		users.put(defaultUser),
+		apps.put({
+			installedApps: defaultApps
+		}),
+		tx.done
+	])
+
+	userStore.$patch({
+		currentUser: defaultUser,
+	});
 }
