84d6bbe4b53326e50fea54e3093fe49168eff2ae
Chiboy <chiboyekene1@gmail.com>
2025-01-23 20:13:41 +0100

    Added state to indicate when an app has an open instance

--- components/Windows/TaskBarIcon.vue
+++ components/Windows/TaskBarIcon.vue
@@ -1,14 +1,19 @@
 <script lang="ts" setup>
 import type { TaskBarIcon } from '#build/imports';
+import { openApp, getAppWindows } from '~/applications';
 const props = defineProps<TaskBarIcon>()
+const openWindows = getAppWindows();
+const hasInstance = computed(() => {
+  return !!openWindows.value.find(win => win.name === props.name)
+})
 </script>
 
 <template>
-  <div class="icon moving w-[50px] h-[50px] relative p-2.5 cursor-pointer flex justify-center" :aria-label="name"
-    :title="name" @contextmenu.prevent="rClick">
+  <div class="icon moving w-[50px] h-[50px] relative p-2.5 cursor-pointer flex justify-center" @click="openApp(name)"
+    :aria-label="name" :title="name" @contextmenu.prevent="rClick">
     <img class="-mt-0.5 w-[25px] h-[25px]" :src="icon" :alt="name" draggable="false" />
 
-    <div class="app-open-indicator" :class="[]"></div>
+    <div class="app-open-indicator" :class="{ 'active': hasInstance }"></div>
   </div>
 </template>
 
