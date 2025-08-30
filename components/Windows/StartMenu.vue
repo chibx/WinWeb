1cc91b5acd552c9e8493cb17cd55e322bb6ad0ef
chibx <chiboyekene1@gmail.com>
2025-08-30 20:41:38 +0100

    Introducing the StartMenu component
    Still a work in progress

---
+++ components/Windows/StartMenu.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import type { CSSProperties } from "vue";
+import type { StartMenuProps } from "~/types/desktop";
+
+const props = defineProps<StartMenuProps>();
+
+// Access start menu icon
+const startMenuIcon = ref<HTMLElement | null>(null);
+const { left } = useElementBounding(startMenuIcon);
+
+watch(left, () => {
+    console.log(left.value)
+}, { immediate: true })
+
+const bgStyles = computed<CSSProperties>(() => {
+    if (startMenuStyles.gradient) {
+        return {
+            // TODO:  Make the gradient possibly and array of gradients
+            backgroundImage: startMenuStyles.gradient,
+        };
+    }
+    const [r, g, b] = startMenuStyles.bgColor;
+
+    return {
+        backgroundColor: `rgba(${r}, ${g}, ${b}, ${startMenuStyles.opacity / 100})`,
+    };
+});
+
+onMounted(() => {
+    startMenuIcon.value = document.querySelector<HTMLElement>(".windows-start-icon");
+})
+
+</script>
+
+<template>
+    <div class="start-menu w-[500px] h-150" :style="{
+        position: 'absolute', left: left + 'px', bottom: '70px',
+        ...bgStyles
+    }">
+
+        <div>
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.start-menu {
+    background-image: linear-gradient(to bottom left, rgb(50, 169, 255) 20%, rgba(0, 149, 255, 0.435) 50%, rgb(122, 200, 255));
+}
+</style>
