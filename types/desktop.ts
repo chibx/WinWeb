c0b8989a22fcc07237f8d26532a325809ec82b7a
chibx <chiboyekene1@gmail.com>
2025-08-30 20:23:58 +0100

    Added start menu types

--- types/desktop.ts
+++ types/desktop.ts
@@ -4,8 +4,8 @@ export type TaskBarIcon = {
     name: string;
     icon: string;
     pinned?: boolean;
-    rClick(): Promise<TaskBarMenu[]>
-}
+    rClick(): Promise<TaskBarMenu[]>;
+};
 
 export type DesktopIcon = {
     /** Name of the resource */
@@ -13,51 +13,64 @@ export type DesktopIcon = {
     /** Icon url to show */
     icon: string;
     /** Function that would be called on right-click  */
-    rClick(): Promise<DesktopIconMenu[]>
-}
+    rClick(): Promise<DesktopIconMenu[]>;
+};
 
-export type DesktopIconWithFocus = [Readonly<ShallowRef<HTMLElement | null>>, Ref<boolean, boolean>]
+export type DesktopIconWithFocus = [Readonly<ShallowRef<HTMLElement | null>>, Ref<boolean, boolean>];
 
 export type DesktopConfig = {
     taskbar: {
-        position: 'top' | 'right' | 'bottom' | 'left',
-        iconPosition: 'center' | 'left',
-        bgColor: string,
-        iconHoverColor: string,
-        shouldHide: boolean
-    }
-}
+        position: "top" | "right" | "bottom" | "left";
+        iconPosition: "center" | "left";
+        bgColor: string;
+        iconHoverColor: string;
+        shouldHide: boolean;
+    };
+};
 
 export type DesktopCoords = {
     left: number;
-    top: number
-}
+    top: number;
+};
 
-export type DragPaneValidator = (func: (ev: MouseEvent) => boolean) => void
+export type DragPaneValidator = (func: (ev: MouseEvent) => boolean) => void;
 
-export type DragPaneCoords = { x: number; y: number, width: number, height: number, left: number, top: number }
+export type DragPaneCoords = { x: number; y: number; width: number; height: number; left: number; top: number };
 
 export type Windows = {
     appName: string;
     /** Unique ID of the instance (Obviously gonna be the key) */
     id: string;
     isMini: boolean;
-
-}
+};
 
 export type TaskBarMenu = {
     group: string;
-    fields: TaskBarMenuField[]
-}
+    fields: TaskBarMenuField[];
+};
 
 type TaskBarMenuField = {
     name: string;
     title?: string;
-    onclick(): void
-}
+    onclick(): void;
+};
 
 export type DesktopIconMenu = {
     name: string;
     onclick?(): void;
-    children?: DesktopIconMenu[]
-}
+    children?: DesktopIconMenu[];
+};
+
+export type StartMenuStyles = {
+    bgColor: [number, number, number];
+    /** Value should be from __0__ to __100__ */
+    opacity: number;
+    /** Gradient has a higher precedence
+     *  Set to none
+     */
+    gradient?: string;
+};
+
+export type StartMenuProps = {
+    open: boolean;
+};
