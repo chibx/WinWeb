72f7ba4ff92afa7c7122c29273e99f6ad16b3fae
chibx <chiboyekene1@gmail.com>
2025-08-30 20:29:09 +0100

    Made some minor changes to prevent magic constants

--- stores/desktop.ts
+++ stores/desktop.ts
@@ -1,4 +1,7 @@
-import { defineStore, acceptHMRUpdate } from "pinia";
+import { acceptHMRUpdate, defineStore } from "pinia";
+
+const DESKTOP_CONFIG_KEY = "desktop-config";
+const START_MENU_CONFIG_KEY = "startMenu";
 
 export const useDesktop = defineStore("desktop", () => {
     const taskbarIcons = reactive<TaskBarIcon[]>([]);
@@ -6,28 +9,28 @@ export const useDesktop = defineStore("desktop", () => {
     // TODO Implement a background ref for this store
     const config = reactive<DesktopConfig>({
         taskbar: {
-            position: 'bottom',
-            iconPosition: 'center',
-            bgColor: '#1b0027',
-            iconHoverColor: '#ffffff41',
-            shouldHide: false
-        }
-    })
+            position: "bottom",
+            iconPosition: "center",
+            bgColor: "#1b0027",
+            iconHoverColor: "#ffffff41",
+            shouldHide: false,
+        },
+    });
     const desktopVars = computed(() => {
         return {
-            '--taskbar-bg': config.taskbar.bgColor,
-            '--icon-hover-color': config.taskbar.iconHoverColor
-        }
-    })
+            "--taskbar-bg": config.taskbar.bgColor,
+            "--icon-hover-color": config.taskbar.iconHoverColor,
+        };
+    });
 
-    useLocalStorage('config', config);
+    useLocalStorage(DESKTOP_CONFIG_KEY, config);
 
     return {
         config,
         taskbarIcons,
         desktopIcons,
         desktopVars,
-    }
+    };
 });
 
 // Hot Module Replacement
@@ -35,3 +38,13 @@ if (import.meta.hot) {
     import.meta.hot.accept(acceptHMRUpdate(useDesktop, import.meta.hot));
 }
 
+export const startMenuStyles = toReactive(
+    useLocalStorage<StartMenuStyles>(START_MENU_CONFIG_KEY, {
+        bgColor: [0, 0, 0],
+        opacity: 0.5,
+        /** Gradient has a higher precedence
+         *  Set to none
+         */
+        gradient: "",
+    })
+);
