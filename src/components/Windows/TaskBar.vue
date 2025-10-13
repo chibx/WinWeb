d9fe8ca31d56901e41a31318cb6e2a256746a536
chibx <111882496+chibx@users.noreply.github.com>
2025-10-13 12:22:08 +0100

    Moved long code into a separate file

--- src/components/Windows/TaskBar.vue
+++ src/components/Windows/TaskBar.vue
@@ -10,121 +10,22 @@ import { Icon } from "@iconify/vue";
 import { useTaskbar } from "@/stores/taskbar";
 import { useStartMenu } from "@/stores/startmenu";
 import { storeToRefs } from "pinia";
-const ICON_SIZE = 50; // 50px
-// const SPACE_AFTER_RIGHT_BAR = 16; // 16px
+import { useTaskbarIconDrag } from "@/composables/taskbar";
 
 const taskbar = useTaskbar();
 const startMenu = useStartMenu();
 const { startMenuIcon: startMenuIconRef } = storeToRefs(startMenu);
 
 // const taskbarEl = useTemplateRef("taskbar");
-const innerBar = useTemplateRef("taskbar-inner");
 // const rightBar = useTemplateRef("taskbar-right");
+const innerBar = useTemplateRef("taskbar-inner");
+useTaskbarIconDrag(innerBar);
 
 const isCentered = computed(() => taskbar.config.iconPosition === "center");
 const { charging, level } = battery;
 const now = useNow();
 const taskbarTime = useDateFormat(now, "h:mm A");
 const taskbarDate = useDateFormat(now, "MM/DD/YYYY");
-
-let initialClientX = 0;
-let initialElX = 0;
-let taskbarIconIndex = -1;
-/** This is meant for virtual repositioning of translated elements */
-let virtualTaskbarIcons: HTMLElement[] = [];
-let isPointerDown = false;
-let focusedTaskbarIcon: HTMLElement | null = null;
-let translateIdx = 0;
-
-useEventListener(document, "pointerdown", (ev) => {
-    if (ev.target && (ev.target as HTMLElement).closest("#taskbar-inner") !== null) {
-        focusedTaskbarIcon = (ev.target as HTMLElement).closest(".icon");
-        if (!focusedTaskbarIcon) return;
-        initialClientX = ev.clientX;
-        initialElX = focusedTaskbarIcon.getBoundingClientRect().left;
-        taskbarIconIndex = Array.from(unref(innerBar)?.querySelectorAll(".icon") || []).findIndex(
-            (el) => el === focusedTaskbarIcon,
-        );
-        translateIdx = taskbarIconIndex;
-        virtualTaskbarIcons = Array.from(unref(innerBar)?.querySelectorAll(".icon") || []);
-        focusedTaskbarIcon.classList.remove("moving");
-        isPointerDown = true;
-    }
-});
-useEventListener(document, "pointerup", async () => {
-    isPointerDown = false;
-    if (!focusedTaskbarIcon) return;
-
-    let translate;
-    if (translateIdx < taskbarIconIndex) {
-        translate = (taskbarIconIndex - translateIdx) * -ICON_SIZE;
-    } else if (translateIdx > taskbarIconIndex) {
-        translate = (translateIdx - taskbarIconIndex) * ICON_SIZE;
-    } else {
-        translate = taskbarIconIndex * ICON_SIZE;
-    }
-    focusedTaskbarIcon.style.transform = `translateX(${translate}px)`;
-    focusedTaskbarIcon = null;
-
-    // Update the reactive state
-    const taskbarItems = stubTaskbarIcons.value.slice();
-    const focusedItem = taskbarItems.splice(taskbarIconIndex, 1);
-    if (focusedItem[0]) {
-        taskbarItems.splice(translateIdx, 0, focusedItem[0]);
-    }
-    stubTaskbarIcons.value = taskbarItems;
-    await nextTick();
-    virtualTaskbarIcons.forEach(async (el) => {
-        el.classList.remove("moving");
-        el.style.removeProperty("transform");
-        await delay(10);
-        el.classList.add("moving");
-    });
-    virtualTaskbarIcons = [];
-});
-
-useEventListener(document, "pointermove", (ev) => {
-    const innerBarEl = unref(innerBar);
-    if (!isPointerDown || !innerBarEl || !focusedTaskbarIcon) {
-        return;
-    }
-
-    const { clientX } = ev;
-    const { left: innerBarLeft, width: innerBarWidth } = innerBarEl.getBoundingClientRect();
-
-    // Translate x
-    let resolvedPos = clientX - initialClientX;
-    const newLeft = initialElX + resolvedPos;
-    if (newLeft < innerBarLeft) {
-        resolvedPos = taskbarIconIndex * -ICON_SIZE;
-    } else if (newLeft + ICON_SIZE > innerBarLeft + innerBarWidth) {
-        resolvedPos = (virtualTaskbarIcons.length - taskbarIconIndex - 1) * ICON_SIZE;
-    }
-    // --------------------------------------------------------
-
-    const inferredLeft = focusedTaskbarIcon.getBoundingClientRect().left - innerBarLeft;
-    const correctLeft = translateIdx * ICON_SIZE;
-
-    if (inferredLeft < correctLeft - 30) {
-        const el = virtualTaskbarIcons.at(Math.max(0, translateIdx - 1));
-        if (el) {
-            const curTranslateX = new DOMMatrixReadOnly(el.style.transform).m41;
-            el.style.transform = `translateX(${curTranslateX + ICON_SIZE}px)`;
-            virtualTaskbarIcons.splice(translateIdx - 1, 0, ...virtualTaskbarIcons.splice(translateIdx, 1));
-            translateIdx -= 1;
-        }
-    } else if (inferredLeft > correctLeft + 30) {
-        const el = virtualTaskbarIcons.at(Math.min(translateIdx + 1, virtualTaskbarIcons.length - 1));
-        if (el) {
-            const curTranslateX = new DOMMatrixReadOnly(el.style.transform).m41;
-            el.style.transform = `translateX(${-ICON_SIZE + curTranslateX}px)`;
-            virtualTaskbarIcons.splice(translateIdx + 1, 0, ...virtualTaskbarIcons.splice(translateIdx, 1));
-            translateIdx += 1;
-        }
-    }
-
-    focusedTaskbarIcon.style.transform = `translateX(${resolvedPos}px)`;
-});
 
 // TODO Handle positioning the taskbar on different screens
 function animateBar(dur: number) {
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const TASKBAR_ICON_SIZE = 50; // 50px
+// export const SPACE_AFTER_RIGHT_BAR = 16; // 16px
