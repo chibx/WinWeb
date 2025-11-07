6e28dd6214fb07edf08a1c73e61d60685460fc70
chibx <111882496+chibx@users.noreply.github.com>
2025-11-07 02:38:02 +0100

    New Commi

---
+++ src/applications/registry/File Explorer/components/ActionPanel.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="w-full bg-white dark:bg-black/80 py-6 border-y border-[#c9c9c9] dark:border-white"></div>
+</template>
+
+<style scoped></style>
