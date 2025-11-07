ac5addf268cd03ee58b8764c89913045857394d6
chibx <111882496+chibx@users.noreply.github.com>
2025-11-07 21:51:01 +0100

    Added Resizable

---
+++ src/applications/registry/File Explorer/components/FileExplorerTab.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import FileControlPanel from "./ControlPanel.vue";
+import FileActionPanel from "./ActionPanel.vue";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable";
+</script>
+
+<template>
+    <FileControlPanel />
+    <FileActionPanel />
+    <div class="h-full">
+        <ResizablePanelGroup class="w-full h-full" direction="horizontal">
+            <ResizablePanel :default-size="22" :min-size="20" :max-size="28">
+                <div class="sidebar"></div>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel><div class="main-view"></div></ResizablePanel>
+        </ResizablePanelGroup>
+    </div>
+</template>
+
+<style scoped></style>
