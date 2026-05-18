a4feb399771c558a2fac97e2ef6fb3332581b202
chibx <111882496+chibx@users.noreply.github.com>
2026-05-19 00:19:40 +0100

    Add icons for file explorer action pane

---
+++ src/components/icons/DetailsPaneIcon.vue
@@ -0,0 +1,15 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path d="M 12 6 L 8 6 A 4 4 0 0 0 4 10 L 4 14 A 4 4 0 0 0 8 18 L 12 18 Z" fill="none" stroke="#767676"
+            stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+
+        <path d="M 12 6 L 16 6 A 4 4 0 0 1 20 10 L 20 14 A 4 4 0 0 1 16 18 L 12 18 Z" fill="none" stroke="#0067B8"
+            stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+
+        <line x1="15" y1="9" x2="17.5" y2="9" stroke="#0067B8" stroke-width="1.5" stroke-linecap="round" />
+        <line x1="15" y1="12" x2="17.5" y2="12" stroke="#0067B8" stroke-width="1.5" stroke-linecap="round" />
+        <line x1="15" y1="15" x2="17.5" y2="15" stroke="#0067B8" stroke-width="1.5" stroke-linecap="round" />
+    </svg>
+</template>
+
+<style scoped></style>
---
+++ src/components/icons/MediumViewIcon.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor"
+        stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+        <rect x="7" y="5" width="10" height="8" rx="2" />
+        <path d="M8 16h8" />
+        <path d="M8 19h8" />
+    </svg>
+</template>
